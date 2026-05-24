72923a3f7c6a46b29305a05abcd5a87c65ee55e0
NatalyaByzova <byzowa.natalya@gmail.com>
2026-05-24 12:48:04 +0500

    добавление сервисного слоя

---
+++ src/main/java/org/example/service/UserSpringService.java
@@ -0,0 +1,22 @@
+package org.example.service;
+
+import org.example.dto.UserRequestDto;
+import org.example.dto.UserResponseDto;
+
+import java.util.List;
+
+public interface UserSpringService {
+
+    UserResponseDto createUser(UserRequestDto request);
+
+    UserResponseDto getUserById(Long id);
+
+    UserResponseDto getUserByEmail(String email);
+
+    List<UserResponseDto> getAllUsers();
+
+    UserResponseDto updateUser(Long id, UserRequestDto request);
+
+    void deleteUser(Long id);
+
+}
