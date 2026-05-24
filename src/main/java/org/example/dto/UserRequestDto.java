6904f812b0510761898b5033ec0a35240b5a89be
NatalyaByzova <byzowa.natalya@gmail.com>
2026-05-24 12:43:20 +0500

    добавление DTO для запроса и ответа

---
+++ src/main/java/org/example/dto/UserRequestDto.java
@@ -0,0 +1,40 @@
+package org.example.dto;
+
+import jakarta.validation.constraints.*;
+
+public class UserRequestDto {
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotNull(message = "Возраст обязателен")
+    @Min(0) @Max(150)
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
